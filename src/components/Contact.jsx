e40af830d13572e6ffbc67924d1f3b4a79c7082b
Marzandi Zahran <marzandi.leta@gmail.com>
2025-06-22 18:53:35 +0700

    Implement Contact Section

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -25,8 +25,8 @@ const Contact = () => {
 
   return (
     <footer id="contact">
-      <img src="/images/footer-right-leaf.png" alt="right-leaf" id="f-right-leaf" />
-      <img src="/images/footer-left-leaf.png" alt="left-leaf" id="f-left-leaf" />
+      <img src="/images/footer-right-leaf.png" alt="right-leaf-img" id="f-right-leaf" />
+      <img src="/images/footer-left-leaf.png" alt="left-leaf-img" id="f-left-leaf" />
 
       <div className="content">
         <h2>Where to Find Us</h2>
